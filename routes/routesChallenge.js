cd2287e6315a57408ba834475f33c8977c339e9b
Pierre <pierre.boudon.27@gmail.com>
2016-10-06 13:13:58 +0200

    * Added Routes for Challenge

--- routes/routesChallenge.js
+++ routes/routesChallenge.js
@@ -26,30 +26,36 @@ if (process.env.NODE_ENV === "test") {
 }
 
 /* Déclaration Models */
-var Availability = sequelize.import("../models/availability")
-var Conversation = sequelize.import("../models/conversation")
-var Correspond = sequelize.import("../models/correspond")
-var Handicap = sequelize.import("../models/handicap")
-var Has_favorite = sequelize.import("../models/has_favorite")
-var Has_handicap = sequelize.import("../models/has_handicap")
-var Has_hours = sequelize.import("../models/has_hours")
-var hour = sequelize.import("../models/hour")
-var Is_interested_by = sequelize.import("../models/is_interested_by")
-var Licence = sequelize.import("../models/licence")
-var Message = sequelize.import("../models/message")
-var Organize = sequelize.import("../models/organize")
-var Place = sequelize.import("../models/place")
-var Sport = sequelize.import("../models/sport")
-var User = sequelize.import("../models/user")
 var Cars = sequelize.import("../models/cars")
 
 module.exports = function(api){
     api.get("/cars", function(req, res, next) {
-        Cars.findAll().then(function(cars) {
+        Cars.findAll({
+            where: {
+                isReserved: false
+            }
+        }).then(function(cars) {
             if (cars.length > 0) {
                 return res.status(200).send(cars)
             } else {
                 return res.status(404).send("No cars found")
+            }
+        })
+    })
+
+    api.put("/cars/reserve/:id", function(req, res, next) {
+        Cars.findOne({
+            idCARS: req.params.id,
+            isReserved: false
+        }).then(function(car) {
+            if (car != undefined && car.idCARS == req.params.id) {
+                car.update({
+                    isReserved: true
+                }).then(function() {
+                    return res.status(200).send("Car reserved !")
+                })
+            } else {
+                return res.status(404).send("No car found for this id. Or maybe is already reserved")
             }
         })
     })
