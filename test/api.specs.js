ec686413343a84cfcb4b7c797553c2318187e946
Pierre <pierre.boudon.27@gmail.com>
2016-10-06 14:53:05 +0200

    * Set PROD env.

--- test/api.specs.js
+++ test/api.specs.js
@@ -2,8 +2,6 @@ process.env.NODE_ENV = "test"
 
 var request = require('supertest')
 var api = require('../index.js')
-var Sequelize = require('sequelize')
-var ENDPOINT = require('../ENDPOINT.js')
 
 describe('Hello World', function() {
     it('should return Hello World', function() {
